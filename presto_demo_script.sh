035b925cc67a09641db17cfd81872409a692be6d
Jeff <j.almache@indizen.com>
2017-10-19 00:17:04 +0200

    Initial commit

---
+++ presto_demo_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PRESTO_VERSION=0.186
+KAFKA_VERSION=0.10.2.1
+CASSANDRA_VERSION=3.11.1
+mkdir presto_demo; cd presto_demo;
+wget https://repo1.maven.org/maven2/com/facebook/presto/presto-server/$PRESTO_VERSION/presto-server-$PRESTO_VERSION.tar.gz
+wget http://apache.rediris.es/cassandra/$CASSANDRA_VERSION/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz 
+wget http://apache.rediris.es/kafka/$KAFKA_VERSION/kafka_2.11-$KAFKA_VERSION.tgz
+wget https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/$PRESTO_VERSION/presto-cli-$PRESTO_VERSION-executable.jar
+tar -xvf presto-server-$PRESTO_VERSION.tar.gz; mv presto-server-$PRESTO_VERSION presto
+tar -xvf apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz; mv apache-cassandra-$CASSANDRA_VERSION cassandra
+tar -xvf kafka_2.11-$KAFKA_VERSION.tgz; mv kafka_2.11-$KAFKA_VERSION kafka
+mv presto-cli-0.186-executable.jar presto-cli; chmod +x presto-cli
+git clone https://github.com/big-data-europe/docker-hive.git
+rm -rf *.tar.gz
+rm -rf *.tgz
